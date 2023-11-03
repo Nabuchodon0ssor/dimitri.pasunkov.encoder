5938cc2d2a657e7d36fdee900cbeee2c310727dc
Dimitri Pasunkov <javarusher01@gmail.com>
2023-11-03 15:56:38 +0100

    test

--- src/main/java/com/javarush/Application.java
+++ src/main/java/com/javarush/Application.java
@@ -3,6 +3,9 @@ package com.javarush;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        while(true){
+            System.out.println("test");
+        }
     }
 }
 
