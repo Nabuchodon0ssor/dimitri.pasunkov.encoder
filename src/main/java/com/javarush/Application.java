d75ec2dd4d18454674c55f3a3675b1c85db96924
Dimitri Pasunkov <javarusher01@gmail.com>
2023-11-03 16:00:15 +0100

    delete hello world

--- src/main/java/com/javarush/Application.java
+++ src/main/java/com/javarush/Application.java
@@ -2,7 +2,7 @@ package com.javarush;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
         while(true){
             System.out.println("test");
         }
