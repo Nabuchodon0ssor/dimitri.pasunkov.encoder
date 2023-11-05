195200773197d69bf9f179d978a3f330acc89872
Dimitri Pasunkov <javarusher01@gmail.com>
2023-11-05 23:12:26 +0100

    console Method corrected (05.11.2023_23:12)

--- src/main/java/com/javarush/Application.java
+++ src/main/java/com/javarush/Application.java
@@ -5,11 +5,11 @@ public class Application {
     public static void main(String[] args) {
 
         Console console = new Console();
-        console.inputText();
+        console.inputInitialText();
         console.inputKey();
 
         CeasarCipher cypher = new CeasarCipher();
-        cypher.encryptInputtedText(SaveInputtedText.inputtedText);
+        cypher.encryptInputtedText(Console.inputtedText);
 
     }
 }
--- src/main/java/com/javarush/Console.java
+++ src/main/java/com/javarush/Console.java
@@ -1,17 +1,34 @@
 package com.javarush;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Console {
-
-    public void inputText(){
+    public static ArrayList<Character> inputtedText = new ArrayList<>();
+    public void inputInitialText(){
         System.out.println("Please input the text to be encrypted. When the whole text is inputted, please write <<END>> from a new line");
-        SaveInputtedText save = new SaveInputtedText();
-        save.saveInputtedText();
-
+        Scanner scan = new Scanner(System.in);
+        while(true) {
+            String line = scan.nextLine();
+            if(line.equals("END")) break;
+            char[] chars = line.toCharArray();
+            for (char ch : chars) {
+                inputtedText.add(ch);
+            }
+            inputtedText.add('\n');
+        }
+        System.out.println("Inputted Text: " + inputtedText);
     }
 
     public void inputKey(){
         System.out.println("Please input the key (from 1 to 25)");
         CeasarCipher key = new CeasarCipher();
         key.initializeKey();
+    }
+    public void inputEncryptedText(){
+        System.out.println("Please input the encrypted text to be decrypted. When the whole text is inputted, please write <<END>> from a new line");
+        SaveInputtedText save = new SaveInputtedText();
+        save.saveInputtedText();
+
     }
 }
