a1fa525376f39a9a7ccd22b8618ad31c7a066107
Dimitri Pasunkov <javarusher01@gmail.com>
2023-11-05 23:33:50 +0100

    encrypt/decrypt Method corrected

--- src/main/java/com/javarush/CeasarCipher.java
+++ src/main/java/com/javarush/CeasarCipher.java
@@ -5,26 +5,45 @@ import java.util.Scanner;
 public class CeasarCipher {
 
     ArrayList<Character> encryptedText = new ArrayList<>();
-    public static int KEY;
+    ArrayList<Character> decryptedText = new ArrayList<>();
+    public static int ENCRYPT_KEY;
+    public static int DECRYPT_KEY;
     Scanner scan = new Scanner(System.in);
-    public void initializeKey() {
-        KEY = scan.nextInt();
-        System.out.println("Inputted Key: " + KEY);
+    public void initializeEncryptKey() {
+        ENCRYPT_KEY = scan.nextInt();
+        System.out.println("Inputted Key: " + ENCRYPT_KEY);
     }
-    public void encryptInputtedText(ArrayList<Character> inputtedText) {
-        for (char chars : inputtedText) {
-            int index = Constants.ALPHABET_EN.indexOf(chars);
-            int encryptIndex = (KEY + index) % 26;
+    public void initializeDecryptKey() {
+        DECRYPT_KEY = scan.nextInt();
+        System.out.println("Inputted Key: " + DECRYPT_KEY);
+    }
+    public void encryptInputtedText(ArrayList<Character> inputtedInitialText) {
+        for (char chars : inputtedInitialText) {
+            int position = Constants.ALPHABET_EN.indexOf(chars);
+            int encryptIndex = (ENCRYPT_KEY + position) % 26;
             encryptedText.add(Constants.ALPHABET_EN.get(encryptIndex));
-
-            if (chars == '\n') {
-                encryptedText.add('\n');
-            }
+//
+//            if (chars == '\n') {
+//                encryptedText.add('\n');
+//            }
         }
         System.out.println("Encrypted Text: " + encryptedText);
     }
 
-    public void decryptInputtedText(ArrayList<Character> inputtedText){
+    public void decryptInputtedText(ArrayList<Character> inputtedEncryptedText){
+        for (char chars : inputtedEncryptedText) {
+            int position = Constants.ALPHABET_EN.indexOf(chars);
+            int decryptIndex = (position - ENCRYPT_KEY) % 26;
+            if(decryptIndex<0){
+                decryptIndex = Constants.ALPHABET_EN.size() + decryptIndex;
+            }
+            decryptedText.add(Constants.ALPHABET_EN.get(decryptIndex));
+//
+//            if (chars == '\n') {
+//                decryptedText.add('\n');
+//            }
+        }
+        System.out.println("Encrypted Text: " + decryptedText);
 
     }
 
--- src/main/java/com/javarush/Console.java
+++ src/main/java/com/javarush/Console.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Console {
-    public static ArrayList<Character> inputtedText = new ArrayList<>();
+    public static ArrayList<Character> inputtedInitialText = new ArrayList<>();
+    public static ArrayList<Character> inputtedEncryptedText = new ArrayList<>();
     public void inputInitialText(){
         System.out.println("Please input the text to be encrypted. When the whole text is inputted, please write <<END>> from a new line");
         Scanner scan = new Scanner(System.in);
@@ -13,22 +14,35 @@ public class Console {
             if(line.equals("END")) break;
             char[] chars = line.toCharArray();
             for (char ch : chars) {
-                inputtedText.add(ch);
+                inputtedInitialText.add(ch);
             }
-            inputtedText.add('\n');
+            inputtedInitialText.add('\n');
         }
-        System.out.println("Inputted Text: " + inputtedText);
+        System.out.println("Inputted Text: " + inputtedInitialText);
     }
 
-    public void inputKey(){
+    public void inputEncryptKey(){
         System.out.println("Please input the key (from 1 to 25)");
         CeasarCipher key = new CeasarCipher();
-        key.initializeKey();
+        key.initializeEncryptKey();
     }
     public void inputEncryptedText(){
         System.out.println("Please input the encrypted text to be decrypted. When the whole text is inputted, please write <<END>> from a new line");
-        SaveInputtedText save = new SaveInputtedText();
-        save.saveInputtedText();
-
+        Scanner scan = new Scanner(System.in);
+        while(true) {
+            String line = scan.nextLine();
+            if(line.equals("END")) break;
+            char[] chars = line.toCharArray();
+            for (char ch : chars) {
+                inputtedEncryptedText.add(ch);
+            }
+            inputtedEncryptedText.add('\n');
+        }
+        System.out.println("Inputted Text: " + inputtedEncryptedText);
+    }
+    public void inputDecryptKey(){
+        System.out.println("Please input the key (from 1 to 25)");
+        CeasarCipher key = new CeasarCipher();
+        key.initializeDecryptKey();
     }
 }
