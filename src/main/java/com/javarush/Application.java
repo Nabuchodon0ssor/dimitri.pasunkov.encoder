e84c2f518c1630034d78087df58f724670723228
Dimitri Pasunkov <149790581+Nabuchodon0ssor@users.noreply.github.com>
2023-11-07 14:18:05 +0100

    Update Application.java

--- src/main/java/com/javarush/Application.java
+++ src/main/java/com/javarush/Application.java
@@ -15,6 +15,29 @@ public class Application {
         console.inputDecryptKey();
         cypher.decryptInputtedText(Console.inputtedEncryptedText, DECRYPT_KEY);
 
+
+        // Добро пожаловать в программу шифрования текста! Если текст, который надо зашифровать/ дешифровать, находится в текстовом файле, введите File, если Вы хотите произвести ввод вручную, введите Input
+                // if File -> Введите путь к файлу с текстом
+                // scan FilePath
+                // save FilePath
+                // Вы хотите зашифровать файл (E), расшифровать с известным ключом (D), либо расшифровать взломом (B)
+                //  E or D
+                // Введите ключ
+                // scan Key
+                // save Key
+                            // If E -> encryptText(FilePath, key)
+                            // saveTextToFile (newFilePath)
+                            // Исходный текст зашифрован и находится в новом файле newFilePath! 
+        
+
+
+
+
+
+
+
+        
+        
     }
 }
 
