ebe0cc33427a6fc4cdba3f2e86686b6833298f7a
Dimitri Pasunkov <149790581+Nabuchodon0ssor@users.noreply.github.com>
2023-11-07 13:58:45 +0100

    Update Texts.java

--- src/main/java/com/javarush/Texts.java
+++ src/main/java/com/javarush/Texts.java
@@ -2,8 +2,8 @@ package com.javarush;
 
 public class Texts {
   public static ArrayList<Character> inputtedInitialText;
-  public static ArrayList<Character> inputtedEncryptedText;
-  public static ArrayList<Character> inputtedDecryptedText;
+  public static ArrayList<Character> inputtedTextEncrypted;
+  public static ArrayList<Character> inputtedTextDecrypted;
   
   public static ArrayList<Character> initialTextFromFile;
   public static ArrayList<Character> encryptedTextFromFile;
