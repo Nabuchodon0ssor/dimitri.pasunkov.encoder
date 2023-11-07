e8124bfe7cc86566e68289a80d499315da9c5538
Dimitri Pasunkov <149790581+Nabuchodon0ssor@users.noreply.github.com>
2023-11-07 10:03:43 +0100

    Update FileService.java
    added method save text from file

--- src/main/java/com/javarush/FileService.java
+++ src/main/java/com/javarush/FileService.java
@@ -1,9 +1,22 @@
 package com.javarush;
 
 public class FileService {
+  public static ArrayList<Character> fileInitialText = new ArrayList<>();
+  public static ArrayList<Character> fileEncryptedText = new ArrayList<>();
   String path = "filepath";
   File file = new File(path);
   Scanner scanner = new Scanner(file);
-  
+    
+  public void saveFileInitialText(){
+        while(scanner.hasNext()) {
+            String line = scan.nextLine();
+            char[] chars = line.toCharArray();
+            for (char ch : chars) {
+                fileInitialText.add(ch);
+            }
+//            fileInitialText.add('\n');
+        }
+        System.out.println("File Text: " + fileInitialText);
+    }
   
 }
